25242b6ce31f809e910dff1258afb052867b8dad
agent-indigo <bradenhynes@gmail.com>
2024-10-02 20:31:07 -0230

    Cleanup & routing

--- app.js
+++ app.js
@@ -1,46 +1,114 @@
-import {createWriteStream} from 'fs'
-import {dirname, join} from 'path'
-import {fileURLToPath} from 'url'
+import 'dotenv/config'
 import express from 'express'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import helmet from 'helmet'
-import hpp from 'hpp'
-import cors from 'cors'
-import rateLimit from 'express-rate-limit'
-import e404handler from './middleware/e404handler'
-import sequelizeErrorHandler from './middleware/sequelizeErrorHandler'
-import connectToSQLdb from './utilities/connectToSQLdb.js'
-connectToSQLdb()
-const app = express()
-app.use(morgan(
-  ':url,:method,:status,:response-time,:date[web]', {
-    stream: createWriteStream(
-      join(
-        dirname(fileURLToPath(import.meta.url)),
-        'log.csv'
-      ), {
-        flags: 'a'
-      }
-    )
+import activitypubExpress from 'activitypub-express'
+import {MongoClient} from 'mongodb'
+const domain = process.env.DOMAIN ?? ''
+const routes = {
+  actor: '/u/:actor',
+  object: '/o/:id',
+  activity: '/s/:id',
+  inbox: '/u/:actor/inbox',
+  outbox: '/u/:actor/outbox',
+  followers: '/u/:actor/followers',
+  following: '/u/:actor/following',
+  liked: '/u/:actor/liked',
+  collections: '/u/:actor/c/:id',
+  blocked: '/u/:actor/blocked',
+  rejections: '/u/:actor/rejections',
+  rejected: '/u/:actor/rejected',
+  shares: '/s/:id/shares',
+  likes: '/s/:id/likes'
+}
+const apex = activitypubExpress({
+  name: 'fedissary',
+  version: '0.1.0',
+  domain,
+  actorParam: 'actor',
+  objectParam: 'id',
+  activityParam: 'id',
+  routes,
+  endpoints: {
+    proxyUrl: `https://${domain}/proxy`
   }
-))
-app.use(express.json())
+})
+const mongoDbClient = new MongoClient(process.env.MONGODB_CXN_STR ?? '')
+const app = express()
+app.use(express.json({
+  type: apex.consts.jsonldTypes
+}))
 app.use(express.urlencoded({
   extended: true
 }))
-app.use(cookieParser())
-app.use(cors())
-app.use(helmet())
-app.use(helmet.xssFilter())
-app.use(hpp())
-app.use(rateLimit({
-  windowMs: 10 * 60 * 1000,
-  max: 100
-}))
-app.use(e404handler)
-app.use(sequelizeErrorHandler)
-app.listen(
+app.route(routes.inbox).get(apex.net.inbox.get).post(apex.net.inbox.post)
+app.route(routes.outbox).get(apex.net.outbox.get).post(apex.net.outbox.post)
+app.get(
+  routes.actor,
+  apex.net.actor.get
+)
+app.get(
+  routes.followers,
+  apex.net.followers.get
+)
+app.get(
+  routes.following,
+  apex.net.following.get
+)
+app.get(
+  routes.liked,
+  apex.net.liked.get
+)
+app.get(
+  routes.object,
+  apex.net.object.get
+)
+app.get(
+  routes.activity,
+  apex.net.activity.get
+)
+app.get(
+  routes.shares,
+  apex.net.shares.get
+)
+app.get(
+  routes.likes,
+  apex.net.likes.get
+)
+app.get(
+  '/.well-known/webfinger',
+  apex.net.webfinger.get
+)
+app.get(
+  '/.well-known/node-info',
+  apex.net.nodeInfoLocation.get
+)
+app.get(
+  '/nodeinfo/:version',
+  apex.net.nodeInfo.get
+)
+app.post(
+  '/proxy',
+  apex.net.proxy.post
+)
+app.on(
+  'apex-outbox',
+  msg => {
+    if (msg.mkactivity.type === 'Create') {
+      console.log(`New ${msg.object.type} from ${msg.actor}`)
+    }
+  }
+)
+app.on(
+  'apex-inbox',
+  msg => {
+    if (msg.mkactivity.type === 'Create') {
+      console.log(`New ${msg.object.type} from ${msg.actor}`)
+    }
+  }
+)
+mongoDbClient.connect().then(() => {
+  apex.store.db = mongoDbClient.db('DB_NAME')
+  return apex.store.setup()
+}).then(() => app.listen(
   8080,
   () => console.log(`Listening on port 8080 in ${process.env.NODE_ENV} mode`)
-)
+))
